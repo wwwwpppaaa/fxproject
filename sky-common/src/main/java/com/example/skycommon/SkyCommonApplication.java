6b05c9be3f9154279b3e5ae6e3d00afb9431f30f
wangpingan <2690011238@qq.com>
2024-06-14 10:56:47 +0800

    demo 1

---
+++ sky-common/src/main/java/com/example/skycommon/SkyCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.skycommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyCommonApplication.class, args);
+    }
+
+}
