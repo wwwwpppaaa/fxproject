aa2318df6d071d21e9628c7d2505985d2a7b4ca6
wangpingan <2690011238@qq.com>
2024-06-21 11:43:25 +0800

    分类管理

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -4,6 +4,7 @@ public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
+
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
--- sky-server/src/main/java/com/sky/service/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.sky.service;
-
-public class HelloService {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("下课去游泳");
-    }
-}
